95343ec2f46fcddcc65c40aba05b95448f9a7998
Danny <dannyvankooten@gmail.com>
2017-07-06 10:18:52 +0200

    use wpml_current_language filter for getting current site lang. closes #2

--- includes/functions.php
+++ includes/functions.php
@@ -11,3 +11,11 @@ function mc4wp_wpml_get_options() {
   $opts = array_merge( $defaults, $opts );
   return $opts;
 }
+
+/**
+* @return string
+*/
+function mc4wp_wpml_get_current_language() {
+  $lang = (string) apply_filters( 'wpml_current_language', null );
+  return $lang;
+}
--- includes/hooks.php
+++ includes/hooks.php
@@ -3,7 +3,7 @@
 add_filter( 'mc4wp_lists', function( $list_ids ) {
   $opts = mc4wp_wpml_get_options();
   $language_map = $opts['language_map'];
-  $language_code = ICL_LANGUAGE_CODE;
+  $language_code = mc4wp_wpml_get_current_language();
 
   if( ! empty( $language_map[$language_code] ) ) {
     return array( $language_map[ $language_code ] );
