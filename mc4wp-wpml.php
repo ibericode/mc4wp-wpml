69aa1c3fb09040963ec27e795053af4e2f69043b
Danny van Kooten <hi@dvk.co>
2025-08-18 08:46:04 +0200

    update copyright year

--- mc4wp-wpml.php
+++ mc4wp-wpml.php
@@ -11,7 +11,7 @@ Domain Path: /languages
 License: GPL v2
 
 Mailchimp for WordPress - WPML Integration
-Copyright (C) 2017-2023, Danny van Kooten, hi@dannyvankooten.com
+Copyright (C) 2017-2025, Danny van Kooten, hi@dannyvankooten.com
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
